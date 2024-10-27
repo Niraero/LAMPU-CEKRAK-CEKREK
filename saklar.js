0f7f8db5738e06a045cf3cfe1041b9a7337d4c77
Nirvana <aeroinsantgenangku@gmail.com>
2024-10-27 13:48:46 +0700

    first commit

---
+++ saklar.js
@@ -0,0 +1,106 @@
+function saklar() {
+	let toggle1 = document.getElementById("default-toggle1")
+	let toggle2 = document.getElementById("default-toggle2")
+	let toggle3 = document.getElementById("default-toggle3")
+	let toggle4 = document.getElementById("default-toggle4")
+	let toggle5 = document.getElementById("default-toggle5")
+	let toggle6 = document.getElementById("default-toggle6")
+	let toggle7 = document.getElementById("default-toggle7")
+	let toggle8 = document.getElementById("default-toggle8")
+	let toggle9 = document.getElementById("default-toggle9")
+	let toggle10 = document.getElementById("default-toggle10")
+
+
+	let lampu1 = document.getElementById("lampu1");
+	let lampu2 = document.getElementById("lampu2");
+	let lampu3 = document.getElementById("lampu3");
+	let lampu4 = document.getElementById("lampu4");
+	let lampu5 = document.getElementById("lampu5");
+	let lampu6 = document.getElementById("lampu6");
+	let lampu7 = document.getElementById("lampu7");
+	let lampu8 = document.getElementById("lampu8");
+	let lampu9 = document.getElementById("lampu9");
+	let lampu10 = document.getElementById("lampu10");
+
+
+	if (toggle1.checked) {
+		// nyalakan lampu
+		lampu1.src = "assets/images/on.gif"
+	} else {
+		// matikan lampu
+		lampu1.src = "assets/images/off (3).gif"
+	}	
+	
+	if (toggle2.checked) {
+		// nyalakan lampu
+		lampu2.src = "assets/images/on.gif"
+	} else {
+		// matikan lampu
+		lampu2.src = "assets/images/off (3).gif"
+	}	
+
+	if (toggle3.checked) {
+		// nyalakan lampu
+		lampu3.src = "assets/images/on.gif"
+	} else {
+		// matikan lampu
+		lampu3.src = "assets/images/off (3).gif"
+	}
+	
+	if (toggle4.checked) {
+		// nyalakan lampu
+		lampu4.src = "assets/images/on.gif"
+	} else {
+		// matikan lampu
+		lampu4.src = "assets/images/off (3).gif"
+	}	
+
+	if (toggle5.checked) {
+		// nyalakan lampu
+		lampu5.src = "assets/images/on.gif"
+	} else {
+		// matikan lampu
+		lampu5.src = "assets/images/off (3).gif"
+	}
+	
+	if (toggle6.checked) {
+		// nyalakan lampu
+		lampu6.src = "assets/images/on.gif"
+	} else {
+		// matikan lampu
+		lampu6.src = "assets/images/off (3).gif"
+	}
+	
+	if (toggle7.checked) {
+		// nyalakan lampu
+		lampu7.src = "assets/images/on.gif"
+	} else {
+		// matikan lampu
+		lampu7.src = "assets/images/off (3).gif"
+	}	
+
+    if (toggle8.checked) {
+		// nyalakan lampu
+		lampu8.src = "assets/images/on.gif"
+	} else {
+		// matikan lampu
+		lampu8.src = "assets/images/off (3).gif"
+	}
+	
+	if (toggle9.checked) {
+		// nyalakan lampu
+		lampu9.src = "assets/images/on.gif"
+	} else {
+		// matikan lampu
+		lampu9.src = "assets/images/off (3).gif"
+	}	
+
+	if (toggle10.checked) {
+		// nyalakan lampu
+		lampu10.src = "assets/images/on.gif"
+	} else {
+		// matikan lampu
+		lampu10.src = "assets/images/off (3).gif"
+	}	
+
+}
